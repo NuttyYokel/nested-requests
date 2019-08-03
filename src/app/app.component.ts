e618b339b53204e730bf5c1978b2d7dbf61ffc49
Marton Cserei <marton.cserei@iquestgroup.com>
2019-08-03 13:06:47 +0300

    with maps and switchmaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { map, switchMap, mergeAll, toArray } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookstore';
+  books$: Observable<any>;
+
+  constructor(http: HttpClient) {
+    this.books$ = http.get('http://localhost:3000/books').pipe(
+      switchMap((books: any[]) => {
+        return http.get('http://localhost:3000/persons').pipe(
+          switchMap((persons: any[]) => {
+            return from(
+              books.map((book) => {
+                const author = persons.find((person) => person.name === book.author.name);
+                return http
+                  .post('http://localhost:3000/nationality', { code: author.nationality })
+                  .pipe(map((nationality) => ({ ...book, author: { ...author, nationality } })));
+              })
+            ).pipe(
+              mergeAll(),
+              toArray()
+            );
+          })
+        );
+      })
+    );
+
+    this.books$.subscribe((response) => console.log(response));
+  }
 }
